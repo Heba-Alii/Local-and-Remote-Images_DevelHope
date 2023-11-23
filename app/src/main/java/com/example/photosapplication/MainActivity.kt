300f6713120d3161f0473d17fc68cf6bde85e5de
Heba-Alii <hebaaliali18@gmail.com>
2023-11-23 16:19:04 +0200

    from remote with Glide

--- app/src/main/java/com/example/photosapplication/MainActivity.kt
+++ app/src/main/java/com/example/photosapplication/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.photosapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.photosapplication.databinding.ActivityMainBinding
 import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    private var imageOneDisplayed = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Glide.with(this).load("https://drive.google.com/uc?id=1WyxFMU2Qnc_zBpSZLHJGkQOSdUgb48l9")
+            .into(binding.remoteImgOne)
+        Glide.with(this).load("https://drive.google.com/uc?id=16nJMZswp5RwJGilX3LlUoxut_yaZdZdZ")
+            .into(binding.remoteImgTwo)
+        Glide.with(this).load("https://drive.google.com/uc?id=1CGKF_ZBWbHcP9oFll4hISG2PoX1WaRrr")
+            .into(binding.remoteImgThree)
+        Glide.with(this).load("https://drive.google.com/uc?id=153t8abVhUdVAi6GscV_LPAwzUwD1p7R6")
+            .into(binding.remoteImgFour)
     }
 }
